3daff4725e580b220644493babd869f056661858
zinhoxxl <zinhoxxl@gmail.com>
2022-01-05 14:49:54 +0900

    22.0105

---
+++ och03_DI/src/main/java/DI08/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package DI08;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+	
+	public Student student1() {
+		ArrayList<String> hobbys = new ArrayList<String>();
+		hobbys.add("수영");
+		hobbys.add("물내리기");
+		
+		Student student = new Student("을지문덕", 55, hobbys);
+		student.setHeight(170);
+		student.setWeight(70);
+		
+		return student;
+		
+	}
+
+}
---
+++ och03_DI/src/main/java/DI08/Student.java
@@ -0,0 +1,62 @@
+package DI08;
+
+import java.util.ArrayList;
+
+public class Student {
+	private String name;
+	private int age;
+	private ArrayList<String> hobbys;
+	private double weight;
+	private double height;
+	
+	// 생성자
+	public Student(String name, int age, ArrayList<String> hobbys) {
+		this.name = name;
+		this.age = age;
+		this.hobbys = hobbys;
+	}
+
+	// getter / setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public ArrayList<String> getHobbys() {
+		return hobbys;
+	}
+
+	public void setHobbys(ArrayList<String> hobbys) {
+		this.hobbys = hobbys;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	
+	
+}
