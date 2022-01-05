8bef200ef50c4b62117a875b4f601e36f9dd3116
zinhoxxl <zinhoxxl@gmail.com>
2022-01-05 15:13:56 +0900

    22.0105

---
+++ och03_DI/src/main/java/DI08/MainClass.java
@@ -0,0 +1,30 @@
+package DI08;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import DI07.Student;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX8.xml");   
+
+		Student student1 = ctx.getBean("student1", Student.class);
+		System.out.println("이름 : " + student1.getName());
+		System.out.println("나이 : " + student1.getAge());
+		System.out.println("취미 : " + student1.getHobbys());
+		System.out.println("신장 : " + student1.getHeight());
+		System.out.println("몸무게 : " + student1.getWeight());
+		
+		Student student2 = ctx.getBean("student2", Student.class);
+		System.out.println("이름 : " + student2.getName());
+		System.out.println("나이 : " + student2.getAge());
+		System.out.println("취미 : " + student2.getHobbys());
+		System.out.println("신장 : " + student2.getHeight());
+		System.out.println("몸무게 : " + student2.getWeight());
+		ctx.close();
+		
+	}
+
+}
