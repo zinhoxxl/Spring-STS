2eb96db3a6c34183c00bdd69373d8b1e275e724e
zinhoxxl <zinhoxxl@gmail.com>
2022-01-05 10:05:54 +0900

    22.0105

--- och03_DI/src/main/java/och03_DI/MyCalculator.java
+++ och03_DI/src/main/java/och03_DI/MyCalculator.java
@@ -5,5 +5,17 @@ public class MyCalculator {
 	private int firstNum;
 	private int secondNum;
 	
+	public void add() {
+		calculator.addition(firstNum, secondNum);
+	}
+	public void sub() {
+		calculator.addition(firstNum, secondNum);
+	}
+	public void mul() {
+		calculator.addition(firstNum, secondNum);
+	}
+	public void div() {
+		calculator.addition(firstNum, secondNum);
+	}
 
 }
