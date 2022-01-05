e44e2ff7112e3fe74027e98d2b17dbb278651dc9
zinhoxxl <zinhoxxl@gmail.com>
2022-01-05 15:45:42 +0900

    22.0105

--- och04_sdlc/src/main/java/sdlc01/OtherStudent.java
+++ och04_sdlc/src/main/java/sdlc01/OtherStudent.java
@@ -28,16 +28,8 @@ public class OtherStudent {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public int getAge() {
 		return age;
 	}
 
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
 }
