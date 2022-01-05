5b8fe04a9a4b577cff7cacc27b948e0576dea7ba
zinhoxxl <zinhoxxl@gmail.com>
2022-01-05 15:52:01 +0900

    22.0105

---
+++ och04_sdlc/src/main/java/sdlc02/Student.java
@@ -0,0 +1,31 @@
+package sdlc02;
+
+public class Student {
+	private String name;
+	private int age;
+
+	// 생성자
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// getter / setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
