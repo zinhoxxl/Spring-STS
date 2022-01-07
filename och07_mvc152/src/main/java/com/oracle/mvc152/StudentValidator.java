efc4f1a4c4833be7d1b7e3eae82b473a3d6583ff
zinhoxxl <zinhoxxl@gmail.com>
2022-01-07 16:08:38 +0900

    22.0107

--- och07_mvc152/src/main/java/com/oracle/mvc152/StudentValidator.java
+++ och07_mvc152/src/main/java/com/oracle/mvc152/StudentValidator.java
@@ -6,12 +6,27 @@ import org.springframework.validation.Validator;
 public class StudentValidator implements Validator {
 
 	@Override
-	public boolean supports(Class<?> clazz) {
-		return false;
+	public boolean supports(Class<?> arg0) {
+		// 검증할 객체의 클래스 타입 정보
+		return Student.class.isAssignableFrom(arg0);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
+		System.out.println("validate()");
+		Student student = (Student)target;
+		
+		String studentName = student.getName();
+		if(studentName == null || studentName.trim().isEmpty()) {
+			System.out.println("studentName is null or empty");
+			errors.rejectValue("name", "이름이 없거나 비어 있습니다");
+		}
+		
+		int studentId = student.getId();
+		if(studentId == 0) {
+			System.out.println("studentId is 0");
+			errors.rejectValue("id", "trouble");
+		}
 
 	}
 
